f9b08ae4f8deba2539596872e834b503d30002ea
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2023-04-13 06:15:22 -0400

    Add Cloud class and Cloud spawning interval

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -63,6 +63,12 @@ export default class DemoScene extends Phaser.Scene {
       { key: "laserSound", url: "assets/sounds/laser.mp3" },
       { key: "hurtSound", url: "assets/sounds/hurt.mp3" },
     ]);
+
+    this.load.atlas(
+      "cloud-atlas",
+      "assets/clouds/cloud-atlas.png",
+      "assets/clouds/cloud-atlas.json"
+    );
   }
 
   create() {
@@ -577,6 +583,80 @@ export default class DemoScene extends Phaser.Scene {
         }
       }
     });
+
+    /**********************************************/
+    // Background Clouds
+    /**********************************************/
+
+    class Cloud extends Phaser.Physics.Arcade.Sprite {
+      constructor(scene: Phaser.Scene) {
+        super(
+          scene,
+          -50,
+          -50,
+          "cloud-atlas",
+          `cloud${Math.floor(Math.random() * 3 + 1)}.png`
+        );
+
+        const worldLeft = scene.physics.world.bounds.left;
+        const worldHeight = scene.physics.world.bounds.height;
+
+        this.setPosition(
+          worldLeft - 32,
+          Math.floor(Math.random() * worldHeight)
+        );
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.setDepth(-1);
+
+        this.setVelocityX(Math.floor(Math.random() * 30) + 15);
+      }
+
+      preUpdate() {
+        const worldRight = this.scene.physics.world.bounds.right;
+        if (this.x > worldRight + 64) {
+          this.destroy();
+        }
+      }
+    }
+
+    class CloudTimer {
+      scene: Phaser.Scene;
+      interval: number;
+      timerEvent: Phaser.Time.TimerEvent | null;
+
+      constructor(scene: Phaser.Scene, interval: number) {
+        this.scene = scene;
+        this.interval = interval;
+        this.timerEvent = null;
+      }
+
+      start() {
+        if (!this.timerEvent) {
+          this.timerEvent = this.scene.time.addEvent({
+            delay: this.interval,
+            loop: true,
+            callback: this.onInterval,
+            callbackScope: this,
+          });
+        }
+      }
+
+      stop() {
+        if (this.timerEvent) {
+          this.timerEvent.destroy();
+          this.timerEvent = null;
+        }
+      }
+
+      onInterval() {
+        new Cloud(this.scene);
+      }
+    }
+
+    new CloudTimer(this, 1250).start();
   }
 
   update(t: number) {
