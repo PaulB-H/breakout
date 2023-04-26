fbc19c685b556fcbfc38629f3220ed74feeaa229
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2023-04-26 01:05:53 -0400

    Removed heatseeking balls when 1 ball left
    To be changed into a power or re-worked

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -694,19 +694,24 @@ export default class BaseScene extends Phaser.Scene {
       this.newBall.x = this.player.x;
     }
 
-    if (this.blocks === 1) {
-      const target = this.blockGroup.children
-        .entries[0] as Phaser.Physics.Arcade.Sprite;
+    /**********************************************/
+    // Heatseeking Balls when 1 left (could be changed into a power or upgrade)
+    // Since we move blocks out of blockGroup before
+    // destruction now this needs updating
+    /**********************************************/
+    // if (this.blocks === 1) {
+    //   const target = this.blockGroup.children
+    //     .entries[0] as Phaser.Physics.Arcade.Sprite;
 
-      this.ballGroup.getChildren().forEach((ball) => {
-        const myBall = ball as Phaser.Physics.Arcade.Sprite;
-        const newX = Phaser.Math.Linear(myBall.x, target.x, 0.075);
-        const newY = Phaser.Math.Linear(myBall.y, target.y, 0.075);
-        myBall.setVelocity(0, 0);
-        myBall.x = newX;
-        myBall.y = newY;
-      });
-    }
+    //   this.ballGroup.getChildren().forEach((ball) => {
+    //     const myBall = ball as Phaser.Physics.Arcade.Sprite;
+    //     const newX = Phaser.Math.Linear(myBall.x, target.x, 0.075);
+    //     const newY = Phaser.Math.Linear(myBall.y, target.y, 0.075);
+    //     myBall.setVelocity(0, 0);
+    //     myBall.x = newX;
+    //     myBall.y = newY;
+    //   });
+    // }
 
     // My "AI"
     // Using "LERP" we get a more natural AI
