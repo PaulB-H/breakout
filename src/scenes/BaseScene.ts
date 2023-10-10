d664bea47c8fd4f38583464f74187eaa351ff9fa
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2023-10-10 04:19:33 -0400

    remove heatseeking balls on 1 block left
    for now

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -836,20 +836,20 @@ Reloading page...`
     // Since we move blocks out of blockGroup before
     // destruction now this needs updating
     /**********************************************/
-    if (this.blocks === 1) {
-      const target = this.blockGroup.children
-        .entries[0] as Phaser.Physics.Arcade.Sprite;
-      if (!target) return;
+    // if (this.blocks === 1) {
+    //   const target = this.blockGroup.children
+    //     .entries[0] as Phaser.Physics.Arcade.Sprite;
+    //   if (!target) return;
 
-      this.ballGroup.getChildren().forEach((ball) => {
-        const myBall = ball as Phaser.Physics.Arcade.Sprite;
-        const newX = Phaser.Math.Linear(myBall.x, target.x, 0.075);
-        const newY = Phaser.Math.Linear(myBall.y, target.y, 0.075);
-        myBall.setVelocity(0, 0);
-        myBall.x = newX;
-        myBall.y = newY;
-      });
-    }
+    //   this.ballGroup.getChildren().forEach((ball) => {
+    //     const myBall = ball as Phaser.Physics.Arcade.Sprite;
+    //     const newX = Phaser.Math.Linear(myBall.x, target.x, 0.075);
+    //     const newY = Phaser.Math.Linear(myBall.y, target.y, 0.075);
+    //     myBall.setVelocity(0, 0);
+    //     myBall.x = newX;
+    //     myBall.y = newY;
+    //   });
+    // }
 
     // My "AI"
     // Using "LERP" we get a more natural AI
