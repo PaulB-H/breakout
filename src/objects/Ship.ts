7acfa6b454ec4e75822c77e751abda96efe5a5ee
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2023-12-13 02:46:46 -0500

    Move ship list object out of Ship class & export
    This way we can use it for the class AND get the list of ship details in other places easily and from a single source

--- src/objects/Ship.ts
+++ src/objects/Ship.ts
@@ -1,17 +1,19 @@
 import { SHEETS } from "../constants";
 
+export const ships = {
+  base: { stopped: 0, left: 10, right: 20 },
+  vanu: { stopped: 1, left: 11, right: 21 },
+  orb: { stopped: 2, left: 12, right: 22 },
+  snowflake: { stopped: 3, left: 13, right: 23 },
+  lightning: { stopped: 4, left: 14, right: 24 },
+};
+
 // This ship sprite (non physics) "attaches" to the
 // platform by matching its x position in preUpdate(){}
 export default class Ship extends Phaser.GameObjects.Sprite {
   platform: Phaser.Physics.Arcade.Sprite;
 
-  ships = {
-    base: { stopped: 0, left: 10, right: 20 },
-    vanu: { stopped: 1, left: 11, right: 21 },
-    orb: { stopped: 2, left: 12, right: 22 },
-    snowflake: { stopped: 3, left: 13, right: 23 },
-    lightning: { stopped: 4, left: 14, right: 24 },
-  };
+  ships = ships;
 
   shipFrames: { stopped: number; left: number; right: number } =
     this.ships.base;
